bcce969ca815defe3d0032504683369e77e86229
Luke Benstead <kazade@gmail.com>
2017-02-10 09:26:21 +0000

    Add some extra error checking to make sure paths are correct

--- gulpify/config.py
+++ gulpify/config.py
@@ -1,5 +1,6 @@
 import os
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 
 NODE_PREFIX = os.environ.get("NODE_PREFIX", settings.NODE_PREFIX)
 NODE_PATH = os.environ.get("NODE_PATH", os.path.join(NODE_PREFIX, "node_modules"))
@@ -10,6 +11,16 @@ GULP_FILE = os.path.join(GULP_PATH, "gulp.js")
 
 
 def generate_gulp_args(task=None):
+    if not os.path.exists(NODE_PATH):
+        raise ImproperlyConfigured(
+            "Unable to find node_modules at: {}".format(NODE_PATH)
+        )
+
+    if not os.path.exists(GULP_CLI_FILE):
+        raise ImproperlyConfigured(
+            "Unable to find gulp-cli at: {}".format(GULP_CLI_FILE)
+        )
+
     args = [GULP_CLI_FILE]
     if task:
         args.append(task)
